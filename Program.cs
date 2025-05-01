b40ec520b5a437d1061f6bc0d2128697d3e012ee
Romch33tos <Romch33tos@yandex.ru>
2025-05-01 19:48:30 +0700

    Основная программа

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Windows.Forms;
+
+static class Program
+{
+  [STAThread]
+  static void Main()
+  {
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+    
+    var view = new DirectorySyncForm();
+    ILogger logger = view.SelectedLogFormat == LogFormat.XML 
+      ? new XmlLogger() 
+      : new JsonLogger();
+    var model = new DirectoryModel(logger);
+    var presenter = new DirectorySyncPresenter(view, model, logger);
+    
+    Application.Run(view);
+  }
+}
